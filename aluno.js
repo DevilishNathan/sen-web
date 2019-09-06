09025c9580153e1f47a217a609893433c501aada
NATHAN SOARES RODRIGUES DURAES <525924437@alunos.etec.itaqua>
2019-09-06 15:22:08 -0300

    Foi realizado requisição assincrona com o servidor API

--- aluno.js
+++ aluno.js
@@ -14,6 +14,11 @@
 
             let botaoQueGira = Ladda.create(botao[0])
             botaoQueGira.start()
+            fetch("http://localhost:8080/sen-api/teste.php").then(function(resposta){
+                return resposta.json()
+            }).then(function(dados){
+                alert(dados.mensagem)
+            })
         })
 
     }
